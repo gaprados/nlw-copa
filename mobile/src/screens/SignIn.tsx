9943dae81295307bb61498671d5c1ebd38df5f46
Gabriel Prado <gaprados3@gmail.com>
2022-11-03 13:54:18 -0300

    feat(mobile): signin page

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,13 +1,24 @@
-import { Center, Text } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
+import { Button } from "../components/Button";
+
+import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text
-        color="white"
-        fontSize={24}
-      >
-        Hello World!
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="Entrar com o Google"
+        type='SECONDARY'
+        leftIcon={<Icon as={Fontisto} name="google" color='white' size='md' />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para a criação de sua conta.
       </Text>
     </Center>
   )
