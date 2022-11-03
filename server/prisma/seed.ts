518f54992a263ebf2f99a6e5a44d1e9b829cbf76
Gabriel Prado <gaprados3@gmail.com>
2022-11-03 12:08:26 -0300

    feat(backend): db structure and web routes

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarUrl: 'https://thispersondoesnotexist.com/image',
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-12T14:30:08.050Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'FR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-12T14:30:08.050Z',
+      firstTeamCountryCode: 'AR',
+      secondTeamCountryCode: 'BR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 1,
+          secondTeamPoints: 3,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
